cfd7736e7fbba015478d3b23e9893969b015f95f
sessiz1 <43515327+sessiz1@users.noreply.github.com>
2018-09-23 22:53:44 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -1,20 +1,38 @@
 const Discord = require('discord.js');
 exports.run = function(client, message, args) {
-
   if (!message.guild) {
-    return message.author.send('`temizle` **komutu sadece sunucularda kullanılabilir.**');
+  const ozelmesajuyari = new Discord.RichEmbed()
+  .setColor(0xD00000)
+  .setTimestamp()
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .addField(':warning: Uyarı :warning:', '`temizle` adlı komutu özel mesajlarda kullanamazsın.')
+  return message.author.sendEmbed(ozelmesajuyari); }
+  if (!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) {
+	const botunmesajyonet = new Discord.RichEmbed()
+    .setColor(0xD97634)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField(':warning: Uyarı :warning:', 'Mesajları silebilmem için `Mesajları Yönet` yetkisine sahip olmalıyım.')
+    return message.author.sendEmbed(botunmesajyonet);
   }
-  let mesajsayisi = parseInt(args.join(' '));
-  if (mesajsayisi.length < 1) return message.channel.send('**Kaç mesaj silmem gerektiğini belirtmedin.**')
-  if (mesajsayisi > 100) return message.channel.send('**__100__** **adetden fazla mesaj silemem!**');
-  message.channel.bulkDelete(mesajsayisi + 1);
-  message.channel.send('**__' + mesajsayisi + '__** **adet mesaj sildim!** ')
+  let messagecount = parseInt(args.join(' '));
+  message.channel.fetchMessages({
+    limit: messagecount
+  }).then(messages => message.channel.bulkDelete(messages));
+    const sohbetsilindi = new Discord.RichEmbed()
+    .setColor(0xD97634)
+    .setTimestamp()
+    .addField('Eylem:', 'Sohbet silme')
+    .addField('Yetkili:', message.author.username)
+    .addField('Sonuç:', `Başarılı`)
+    return message.channel.sendEmbed(sohbetsilindi);
+    console.log("Sohbet " + message.member + " tarafından silindi!");
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['sil'],
+  aliases: [],
   permLevel: 2
 };
 
