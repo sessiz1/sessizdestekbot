ca5c291fd0f6e5bf7239e45ea1c84861addfe2fa
sessiz1 <43515327+sessiz1@users.noreply.github.com>
2018-09-24 22:29:46 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -13,7 +13,15 @@ exports.run = (client, message) => {
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('İşte Davet Linkim: https://discordapp.com/api/oauth2/authorize?client_id=423868265677324299&permissions=8&scope=bot Buda Bizim Sunucunun :)  https://discord.gg/yq69bGz');
+    .setDescription('İşte Davet Linkim: https://discordapp.com/api/oauth2/authorize?client_id=492345428608679956&permissions=8&scope=bot Buda Bizim Sunucunun :)  https://discord.gg/7WRwWdn
+BİR SUNUCUYA KATILMAK İÇİN DAVET EDİLDİN
+
+| ★ＳＥＳＳＩＺ★ＡＲＭＹ ★| +293
+28 Online
+286 Members
+
+Katıldı
+');
     return message.author.sendEmbed(pingozel)
 };
 
