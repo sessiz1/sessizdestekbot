e884f9067a385c09702e7e1c3c875a2b11b3876a
sessiz1 <43515327+sessiz1@users.noreply.github.com>
2018-09-26 22:09:43 +0300

    Add files via upload

--- komutlar/duyuru.js
+++ komutlar/duyuru.js
@@ -1,25 +1,27 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-	let mesaj = args.slice(0).join(' ');
-	if (mesaj.length < 1) return message.reply('Yazmam İçin Birşey Yazmalısın!');
+	let guild = message.guild
+	let duyurular = guild.channels.find('name', 'duyurular');
+	if (!duyurular) return message.reply('`duyurular` kanalını bulamıyorum.');
+    let mesaj = args.slice(0).join(' ');
+    if (mesaj.length < 1) return message.reply('Bir şey yazmadınız.');
     message.delete();
     const embed = new Discord.RichEmbed()
-    .setAuthor('')
-    .setColor(3447003)
-    .setDescription(`${mesaj}`)
-    return message.channel.sendEmbed(embed);
+    .setColor(0xD97634)
+    .setDescription(`:anchor: **Tüm Herkese Duyurulur ;**\n${mesaj}`)
+    return guild.channels.get(duyurular.id).sendEmbed(embed);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['duyuryap', 'duyur'],
-  permLevel: 2
+  aliases: ['duyuru', 'duyuruyap'],
+  permLevel: 0
 };
 
 exports.help = {
-  name: 'duyuru',
-  description: 'Güzel Bir Duyuru Görünümü Sağlar.',
-  usage: 'duyuru [Duyuruda Yazıcak Şey]'
+  name: 'duyuruyap',
+  description: 'Sunucuda Duyuru yapmanızı sağlar.',
+  usage: 'duyuruyap [yazı]'
 };
